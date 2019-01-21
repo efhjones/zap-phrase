fbf65339f83eba558f037b81e736584cfea9f108
Emily Jones <efhjones@gmail.com>
2019-01-22 00:30:36 +0100

    finally fix sockets

--- server.js
+++ server.js
@@ -1,9 +1,42 @@
 const express = require("express");
-const app = express();
-const port = process.env.PORT || 5000;
+const http = require("http");
+const socketIO = require("socket.io");
 
-// console.log that your server is up and running
-app.listen(port, () => console.log(`Listening on port ${port}`));
+// our localhost port
+const port = 5000;
+
+const app = express();
+
+// our server instance
+const server = http.createServer(app);
+
+// This creates our socket using the instance of the server
+const io = socketIO(server);
+
+// This is what the socket.io syntax is like, we will work this later
+io.on("connection", socket => {
+  console.log("User connected");
+
+  socket.on("set next player", player => {
+    // once we get a 'change color' event from one of our clients, we will send it to the rest of the clients
+    // we make use of the socket.emit method again with the argument given to use from the callback function above
+    console.log("Player changed to:  ", player);
+    io.sockets.emit("player changed", player);
+  });
+
+  socket.on("set next phrase", phrase => {
+    // once we get a 'change color' event from one of our clients, we will send it to the rest of the clients
+    // we make use of the socket.emit method again with the argument given to use from the callback function above
+    console.log("Phrase changed to:  ", phrase);
+    io.sockets.emit("phrase changed", phrase);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
 
 const baseApi = "/api";
 // create a GET route
