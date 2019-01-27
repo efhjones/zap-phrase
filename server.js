45fa223525854e54a430cfa051d4490518dda710
Emily Jones <efhjones@gmail.com>
2019-01-27 22:53:04 +0100

    i dunno maybe this

--- server.js
+++ server.js
@@ -132,7 +132,7 @@ io.on(handlers.CONNECTION, socket => {
 
 app.use("/api/", api);
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
 
