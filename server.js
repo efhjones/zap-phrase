968c6cc4c181463c1d0340f1c49f98c3ed6eae5d
Emily Jones <efhjones@gmail.com>
2019-01-27 21:35:11 +0100

    welp, let's see if this works

--- server.js
+++ server.js
@@ -7,13 +7,17 @@ require("dotenv").config();
 
 const handlers = require("./socket/handlers.js");
 
-// our localhost port
-const port = 5000;
-
 const app = express();
 
 // our server instance
 const server = http.createServer(app);
+
+app.set("port", process.env.PORT || 5000);
+
+// Express only serves static assets in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
 // This creates our socket using the instance of the server
 const io = socketIO(server);
@@ -127,4 +131,6 @@ io.on(handlers.CONNECTION, socket => {
 
 app.use("/api/", api);
 
-server.listen(port, () => console.log(`Listening on port ${port}`));
+server.listen(app.get("port"), () =>
+  console.log(`Listening on port ${app.get("port")}`)
+);
