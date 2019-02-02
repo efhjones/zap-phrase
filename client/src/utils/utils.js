0e26fc1c949bca8e31864968cbf980d4abf659c8
Emily Jones <efhjones@gmail.com>
2019-02-02 03:35:31 +0100

    create util for preparing game

--- client/src/utils/utils.js
+++ client/src/utils/utils.js
@@ -8,13 +8,23 @@ export const getLocalStorage = key => {
   return localStorage.getItem(key);
 };
 
-export const parseGame = game => {
+const parseGame = game => {
   const teams = game.teams || "[]";
   const phrases = game.phrases || "{}";
   return {
     id: game.id,
     teams: JSON.parse(teams),
     phrases: JSON.parse(phrases)
+  };
+};
+
+export const prepareGameForState = game => {
+  const parsedGame = parseGame(game);
+  return {
+    id: parsedGame.id,
+    isActive: Boolean(game.isActive),
+    teams: parsedGame.teams,
+    phrases: parsedGame.phrases
   };
 };
 
