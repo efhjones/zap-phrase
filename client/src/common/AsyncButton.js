b15bec5c4c20d56a8e009685b6c3b453d683417b
Emily Jones <efhjones@gmail.com>
2019-02-02 03:42:48 +0100

    switch AsyncButton to a SFC

--- client/src/common/AsyncButton.js
+++ client/src/common/AsyncButton.js
@@ -1,19 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 import Spinner from "./Spinner";
 
-class AsyncButton extends Component {
-  render() {
-    const { props } = this;
-    return (
-      <button
-        disabled={props.disabled}
-        className={props.className}
-        onClick={props.onClick}
-      >
-        {props.isLoading ? <Spinner color={props.color} /> : props.children}
-      </button>
-    );
-  }
-}
+const AsyncButton = props => {
+  return (
+    <button
+      disabled={props.disabled}
+      className={props.className}
+      onClick={props.onClick}
+    >
+      {props.isLoading ? <Spinner color={props.color} /> : props.children}
+    </button>
+  );
+};
 
 export default AsyncButton;
