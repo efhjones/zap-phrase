58479d6d8dfe78fd63e107ea57c270dae2ef1160
Emily Jones <efhjones@gmail.com>
2019-02-04 02:19:03 +0100

    attempt to fix failed get for prod server

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.set("port", process.env.PORT || 5000);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
+  app.use("/", express.static(path.join(__dirname, "client/build")));
 }
 
 const io = socketIO(server);
