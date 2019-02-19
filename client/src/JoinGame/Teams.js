a6733ae6a421f123d456524e0ab9250571509475
Emily Jones <efhjones@gmail.com>
2019-02-19 02:12:17 +0100

    let's add some color!

--- client/src/JoinGame/Teams.js
+++ client/src/JoinGame/Teams.js
@@ -1,12 +1,21 @@
 // @flow
 import React from "react";
 
+const teamColors = {
+  1: "#ff4a00",
+  2: "#7678ed"
+};
+
 const Teams = ({ teams, name }) => {
   return (
     <div className="teams">
       {teams.map(team => {
         return (
-          <div key={team.id} className="team">
+          <div
+            key={team.id}
+            className="team"
+            style={{ color: teamColors[team.id] }}
+          >
             <h2>Team {team.id}</h2>
             <ul className="team-members">
               {team.players.map((player, i) => {
