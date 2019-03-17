da7a6e45272892ec00158585f04cbc9b037bbe5f
Emily Jones <efhjones@gmail.com>
2019-03-17 14:20:05 +0100

    handle player spinner animation

--- client/src/Game/PlayerLineup.js
+++ client/src/Game/PlayerLineup.js
@@ -9,15 +9,28 @@ const Player = ({ name, isCurrentPlayer }) => (
   </p>
 );
 
-const PlayerLineup = ({ playerLineup, currentPlayer }) => (
-  <div>
-    {playerLineup.map((player, i) => (
-      <Player
-        name={player.name}
-        isCurrentPlayer={currentPlayer && currentPlayer.name === player.name}
-      />
-    ))}
-  </div>
-);
+const PlayerLineup = ({ playerLineup, currentPlayer }) => {
+  const playerNames = playerLineup.map(player => player.name);
+  const indexOfPlayer = playerNames.indexOf(
+    currentPlayer ? currentPlayer.name : ""
+  );
+  const initialOffset = 30;
+  const playerOffset = initialOffset - indexOfPlayer * 35;
+  const offset = playerOffset - 20;
+  return (
+    <div className="player-lineup">
+      <div className="player-spinner" style={{ top: `${offset}px` }}>
+        {playerLineup.map((player, i) => (
+          <Player
+            name={player.name}
+            isCurrentPlayer={
+              currentPlayer && currentPlayer.name === player.name
+            }
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default PlayerLineup;
