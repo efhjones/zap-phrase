dfe4cc922141b51e6f78bd6401c63452f22e3f73
Emily Jones <efhjones@gmail.com>
2019-03-17 14:32:05 +0100

    use shorthand fragment instead of import

--- client/src/JoinGame/index.js
+++ client/src/JoinGame/index.js
@@ -1,5 +1,5 @@
 // @flow
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import Loading from "../common/Loading";
 import Button from "../common/Button/Button";
 import AsyncButton from "../common/Button/AsyncButton";
@@ -69,7 +69,7 @@ class JoinGame extends Component {
     return teams.length === 0 ? (
       <Loading />
     ) : (
-      <Fragment>
+      <>
         <ZapPhraseTitle />
         <div className="vertical-section">
           <div className="invite-and-join-section">
@@ -88,10 +88,10 @@ class JoinGame extends Component {
                       "validation-failed"}`}
                   >
                     {!canUseName && (
-                      <Fragment>
+                      <>
                         <span>Sorry, that name’s taken.</span>
                         <span>Choose another?</span>
-                      </Fragment>
+                      </>
                     )}
                     <input
                       className="name-field"
@@ -136,7 +136,7 @@ class JoinGame extends Component {
             <Teams teams={teams} name={name} />
           </div>
         </div>
-      </Fragment>
+      </>
     );
   }
 }
