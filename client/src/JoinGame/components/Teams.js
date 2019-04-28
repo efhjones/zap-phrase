7027e432ae02c9654ec3429e7dfa8d043ef56efd
Emily Jones <efhjones@gmail.com>
2019-04-28 22:38:22 +0200

    break up Teams components

--- client/src/JoinGame/components/Teams.js
+++ client/src/JoinGame/components/Teams.js
@@ -1,31 +1,36 @@
 // @flow
 import React from "react";
 
+const Player = ({ name, isMe }) => (
+  <li className={`team-member ${isMe ? "is-me" : ""}`}>{name}</li>
+);
+
+const Team = ({ team, name, i }) => {
+  return (
+    <div key={team.id} className="team" id={`team-${i + 1}`}>
+      <div className="team-header" id={`team-header-${i + 1}`}>
+        <h2>Team {team.id}</h2>
+      </div>
+      <ul className="team-members">
+        {team.players.map(player => {
+          return (
+            <Player
+              key={player.socketId}
+              name={player.name}
+              isMe={player.name === name}
+            />
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
 const Teams = ({ teams, name }) => {
   return (
     <div className="teams">
       {teams.map((team, i) => {
-        return (
-          <div key={team.id} className="team" id={`team-${i + 1}`}>
-            <div className="team-header" id={`team-header-${i + 1}`}>
-              <h2>Team {team.id}</h2>
-            </div>
-            <ul className="team-members">
-              {team.players.map((player, i) => {
-                return (
-                  <li
-                    key={`${player.id}${i}`}
-                    className={`team-member ${
-                      player.name === name ? "is-me" : ""
-                    }`}
-                  >
-                    {player.name}
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-        );
+        return <Team key={team.id} team={team} name={name} i={i} />;
       })}
     </div>
   );
