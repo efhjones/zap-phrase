e448eaafb7182982788a38c4211b5030bc455b7e
Emily Jones <efhjones@gmail.com>
2019-04-28 22:21:25 +0200

    map over categories

--- client/src/JoinGame/components/categories/Categories.js
+++ client/src/JoinGame/components/categories/Categories.js
@@ -3,6 +3,18 @@ import React from "react";
 
 import "./Categories.css";
 
+const categories = [
+  "Random",
+  "Millennials",
+  "Around the House",
+  "Food and Drink",
+  "Plants and Animals",
+  "Idioms",
+  "Entertainment",
+  "Tech and Inventions",
+  "Geography"
+];
+
 const Categories = ({ onSelectCategory, category }) => {
   return (
     <div className="category-dropdown">
@@ -16,15 +28,9 @@ const Categories = ({ onSelectCategory, category }) => {
         }}
         value={category}
       >
-        <option value="Random">Random</option>
-        <option value="Millennials">Millennials</option>
-        <option value="Around the House">Around the House</option>
-        <option value="Food and Drink">Food and Drink</option>
-        <option value="Plants and Animals">Plants and Animals</option>
-        <option value="Idioms">Idioms</option>
-        <option value="Entertainment">Entertainment</option>
-        <option value="Tech and Inventions">Tech and Inventions</option>
-        <option value="Geography">Geography</option>
+        {categories.map(category => (
+          <option value={category}>{category}</option>
+        ))}
       </select>
     </div>
   );
